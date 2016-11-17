751e8ae5864ce002a64d11d28a1755d1e2fcb10b
7rolmstead <7rolmstead@stpatrick.org>
2016-11-17 10:52:07 -0600

    working on instance functions

---
+++ Pizza-master/pizza.js
@@ -0,0 +1,35 @@
+class pizza{
+  //type constructor below
+  constructor(s,mt,vt){
+    this.size = s;
+    this.meatToppings = mt;
+    this.veggieToppings = vt;
+  }
+
+
+  //type instance functions below
+  this.sizeCost = function(){
+    if(s = "Small";){
+      price + 7.99;
+    }
+    else if(s = "Medium";){
+      price + 9.99;
+    }
+    else if(s = "Large";){
+      price + 12.99;
+    }
+    else(s = "Extra-Large"){
+      price + 15.99;
+    }
+  }
+
+  this.toppingCost = function(top){
+    if(top = "Meat";){
+      
+    }
+  }
+
+  //type class functions below
+
+
+}
